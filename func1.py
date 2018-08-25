353d493ffd48009b01eb6c3cb246cc28ee24f3ed
AS Exhibit <archana@Archanas-MacBook-Pro.local>
2018-08-25 13:41:00 -0700

    Added sample python function

---
+++ func1.py
@@ -0,0 +1,14 @@
+def ave1 (no): 
+	sum=0
+	a=0
+	for i in no:
+		sum = sum+ i
+	a= len(no)
+	return float(sum/a)
+
+
+
+
+
+nolist = [1,2,3,4,5]
+print(ave1(nolist)).  # Calling function fun_name(paramerters)
